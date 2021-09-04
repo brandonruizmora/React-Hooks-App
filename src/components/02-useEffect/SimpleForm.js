6cfa93a187f295f81122815d7c20588064f0d86c
brandonruizmora <brandon_ruizmor@outlook.es>
2021-09-03 21:54:09 -0500

    useEffect and Forms

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+
+    const { name, email } = formState;
+
+    useEffect(() => {
+        console.log('useEffect Initial load');
+    }, []);
+
+    useEffect(() => {
+        console.log('useEffect formState change');
+    }, [formState]);
+
+    const handleInputChange = function ({ target }) {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        });
+    }
+
+    return (
+        <div className="container mt-5">
+            <h1>useEffect</h1>
+            <hr />
+            <div class="mb-3">
+                <input type="text" className="form-control" placeholder="tu nombre" autoComplete="off" name="name" value={ name } onChange={ handleInputChange } />
+            </div>
+            <div class="mb-3">
+                <input type="email" className="form-control" placeholder="tucorreo@gmail.com"  autoComplete="off" name="email" value={ email } onChange={ handleInputChange } />
+            </div>
+        </div>
+    );
+}
