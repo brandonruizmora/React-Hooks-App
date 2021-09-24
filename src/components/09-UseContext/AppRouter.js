73ba21ab09af258f57b0373ecd60c6cddfc51329
brandonruizmora <brandon_ruizmor@outlook.es>
2021-09-24 14:40:53 -0500

    navlink

--- src/components/09-UseContext/AppRouter.js
+++ src/components/09-UseContext/AppRouter.js
@@ -2,20 +2,24 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
   } from 'react-router-dom';
   import { HomeScreen } from './HomeScreen';
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
     return (
         <Router>
             <div className="container">
+                <NavBar />
                 <Switch>
                     <Route path="/" exact component={HomeScreen} />
                     <Route path="/about" exact component={AboutScreen} />
                     <Route path="/login" exact component={LoginScreen} />
+                    <Redirect to="/" />
                 </Switch>
             </div>
         </Router>
