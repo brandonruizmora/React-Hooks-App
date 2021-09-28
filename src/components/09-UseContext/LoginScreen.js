e52f3e13eee9230abd43c78d5b9265781ca89fb6
brandonruizmora <brandon_ruizmor@outlook.es>
2021-09-27 21:14:08 -0500

    Using useState, and useContext hooks

--- src/components/09-UseContext/LoginScreen.js
+++ src/components/09-UseContext/LoginScreen.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+
+    const { setUser } = useContext(UserContext);
+
     return (
         <div className="container">
             <h1>LoginScreen</h1>
             <hr />
+            <button 
+            className="btn btn-primary"
+            onClick={() => {
+                setUser({
+                    id: 123,
+                    name: 'Brandon',
+                    email: 'brandon_ruizmor@outlook.es'
+                });
+            }}
+            >
+                Login
+                </button>
         </div>
     )
 }
