670fc86e8f9ae8716b553484a02d13ab13b03e4c
brandonruizmora <brandon_ruizmor@outlook.es>
2021-09-27 20:54:37 -0500

    CreateContext UseContext first steps

--- src/components/09-UseContext/MainApp.js
+++ src/components/09-UseContext/MainApp.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import { AppRouter } from './AppRouter'
+import React from 'react';
+import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
+
 
 export const MainApp = () => {
+    const user = {
+        id: 1234,
+        email: 'brandon_ruizmor@outook.es',
+        name: 'Brandon Ruiz Morales'
+    }
     return (
-        <AppRouter />
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
     )
 }
